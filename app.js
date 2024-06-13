5d822d0a6094ef1f27e2320c2c099ff3c86aceba
CodinJohn <johnathon.collado@gmail.com>
2024-06-13 10:41:03 -0400

    The beginning of Quiz

---
+++ app.js
@@ -0,0 +1,25 @@
+/*-------------- Constants -------------*/
+
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+// Function for Category Selection 
+// Function for answer choice
+// Function for try again 
+// Function for Winner
+// Function for Loser!
+
+/*----------- Event Listeners ----------*/
+
+
+
+// Event listener for Category choice at Landing page.
+// Event listener for answer choice for each question. 
+// Event Listener for try again at the end of quiz. 
+
+console.log('Wussup!')
