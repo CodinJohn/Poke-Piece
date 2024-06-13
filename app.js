972facd63f82f89c71f94e986deaf12638324185
CodinJohn <johnathon.collado@gmail.com>
2024-06-13 12:30:00 -0400

    Landing page coming together

--- app.js
+++ app.js
@@ -5,7 +5,10 @@
 
 
 /*----- Cached Element References  -----*/
-
+const pokemonBtnEl = document.querySelector('.btn1');
+const onepieceBtnEl = document.querySelector('.btn2');
+const answerBtnEls = document.querySelectorAll('.answer');
+const tryAgainBtnEl = document.querySelector('.display');
 
 /*-------------- Functions -------------*/
 // Function for Category Selection 
@@ -14,12 +17,27 @@
 // Function for Winner
 // Function for Loser!
 
+const UpdatePMquestions = (event) => {
+    if(pokemonBtnEl.addEventListener === true) {
+        button.style.display = "none";
+    }
+};
+
+// const updatetoPMquestions = 
+
+// const updatequestions = 
+
 /*----------- Event Listeners ----------*/
+pokemonBtnEl.addEventListener('click', UpdatePMquestions);
 
+// onepieceBtnEl.addEventListener('click' updatetoOPquestions);
 
+// const answerBtnEls.forEach((answerBtnEl) => {
+//     answerBtnEl.addEventListener('click', updatequestions)
+// });
 
 // Event listener for Category choice at Landing page.
 // Event listener for answer choice for each question. 
 // Event Listener for try again at the end of quiz. 
 
-console.log('Wussup!')
+// console.log('Wussup!')
